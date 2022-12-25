aaece094cb721a505279658c202cd2e1565c33ce
John Alexander <105305546+the-vegetarian-vampire@users.noreply.github.com>
2022-12-25 17:44:22 -0500

    Update single_linked_list.py

--- single_linked_list.py
+++ single_linked_list.py
@@ -107,6 +107,29 @@ class LinkedList:
             prev_node.next_node = new
             new.next_node = next_node
 
+    def remove(self, key):
+        """
+        removes node containing daya that matches the key
+        returns the node or None if the key doesn't exist
+        takes O(n) time
+        """
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+
+            return current
+
     def __repr__(self):
         """
         Return a string representation of the list.
